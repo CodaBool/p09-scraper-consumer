42138a8796e8f09623abffa39ff9f3ac66128b36
Coda Bool <codabool@pm.me>
2021-08-23 20:04:51 -0400

    first commit

---
+++ constants/index.js
@@ -0,0 +1,2 @@
+export const LIST_OF_ENDPOINTS = ['trending-github', 'upcoming-movies', 'trending-movies', 'trending-tv', 'upcoming-games', 'trending-npm-1', 'trending-npm-2']
+export const BASE_API_ENDPOINT = 'https://j8xl9nv9k9.execute-api.us-east-1.amazonaws.com/main/api/'
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
+import Head from 'next/head'
+import Navigation from '../components/Navigation'
+// import Footer from '../components/Footer'
+import Container from 'react-bootstrap/Container'
+
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <div className="site">
+      <Head>
+        <title>Web Scrapper</title>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Web Scrapper" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.gif" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.gif" />
+        <link rel="apple-touch-icon" href="/image/favicon-32x32.gif" />
+        <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+      </Head>
+      <Navigation />
+      <main className="site-content">
+        <Container fluid={'md'} className="p-0">
+          <Component {...pageProps} />
+        </Container>
+      </main>
+      {/* <Footer /> */}
+    </div>
+  )
 }
-
-export default MyApp
