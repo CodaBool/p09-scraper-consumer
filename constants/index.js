2ee31e80ccd7ee9bb8e8631fa0bb5904bb8b72dc
CodaBool <CodaBool@pm.me>
2021-10-06 19:14:19 -0400

    remove comments

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,2 @@
 export const LIST_OF_ENDPOINTS = ['trending-github', 'upcoming-movies', 'trending-movies', 'trending-tv', 'upcoming-games', 'trending-npm-1', 'trending-npm-2']
-// export const BASE_API_ENDPOINT = 'https://j8xl9nv9k9.execute-api.us-east-1.amazonaws.com/main/api/'
 export const BASE_API_ENDPOINT = 'https://API_GATEWAY_URL_HERE'
